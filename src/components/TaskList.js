a8f6c86118012973a204df472c70c65e7f1ba67a
Chris Kim <chris.kim@kareo.com>
2020-03-22 19:25:28 -0700

    Added TaskList and story

---
+++ src/components/TaskList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Task from './Task';
+
+function TaskList({ loading, tasks, onPinTask, onArchiveTask}) {
+    const events = {
+        onPinTask,
+        onArchiveTask,
+    };
+
+    if (loading) {
+        return <div className="list-items">loading</div>;
+    }
+
+    return (
+        <div className="list-items">
+            {tasks.map(task => (
+                <Task key={task.id} task={task} {...events} />
+            ))}
+        </div>
+    );
+}
+
+export default TaskList;
